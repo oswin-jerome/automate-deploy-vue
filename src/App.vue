35162a29a821ca2aaeb7b4645861113d301d1d6e
Oswin Jerome <oswinjeromej@gmail.com>
2021-04-22 22:24:34 +0530

    Title added

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,13 @@
  *   All rights reserved.
  */
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to the site"/>
+  <h1> IDEAUX </h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
